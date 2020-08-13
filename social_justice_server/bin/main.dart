1ce40d17a593bd460ddd127f9f4e38ff053043cf
mook <mehek@live.com>
2020-08-13 13:07:55 -0700

    first

---
+++ social_justice_server/bin/main.dart
@@ -0,0 +1,13 @@
+import 'package:social_justice_server/social_justice_server.dart';
+
+Future main() async {
+  final app = Application<SocialJusticeServerChannel>()
+      ..options.configurationFilePath = "config.yaml"
+      ..options.port = 8888;
+
+  final count = Platform.numberOfProcessors ~/ 2;
+  await app.start(numberOfInstances: count > 0 ? count : 1);
+
+  print("Application started on port: ${app.options.port}.");
+  print("Use Ctrl-C (SIGINT) to stop running the application.");
+}
---
+++ social_justice_server/lib/social_justice_server.dart
@@ -0,0 +1,11 @@
+/// social_justice_server
+///
+/// A Aqueduct web server.
+library social_justice_server;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
